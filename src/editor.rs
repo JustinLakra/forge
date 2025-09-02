b7ed6673a98d5e0cdedf97afd9ff76159a42b83b
JustinLakra <justinlakradec10@gmail.com>
2025-09-03 02:36:41 +0530

    ok somewhat works

--- src/editor.rs
+++ src/editor.rs
@@ -7,83 +7,69 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::{stdout, Write};
 
 #[derive(PartialEq, Eq)]
-enum Mode {
+pub enum Mode {
     Normal,
     Editing,
 }
 
 pub struct Editor {
-    mode: Mode,
-    buffer: Vec<char>,
+    pub mode: Mode,
+    pub buffer: Vec<char>,
     cursor: Cursor,
 }
-//TODO make a display function to first clear the terminal screen and then display the buffer
-//elements
 //NOTE fix the imports
 impl Editor {
     pub fn default() -> Self {
         Editor {
-            mode: Mode::Normal, //0 for normal, 1 for visual, 2 for insert
+            mode: Mode::Normal,
             buffer: Vec::new(),
             cursor: Cursor::default(),
         }
     }
     pub fn run(&mut self) {
-        enable_raw_mode().unwrap();
-        execute!(stdout(), Clear(ClearType::All)).ok();
-        execute!(stdout(), MoveTo(0, 0)).ok();
-        loop {
-            match read() {
-                Ok(Key(event)) => {
-                    if let Char(c) = event.code {
-                        if self.mode == Mode::Normal {
-                            if c == 'q' {
-                                break;
-                            }
-                            if c == 'i' {
-                                self.mode = Mode::Editing;
-                            }
-                            //TODO use custom keybindings. create another directory
-                            if c == 'w' {
-                                self.cursor.move_up();
-                            }
-                            if c == 'a' {
-                                self.cursor.move_left();
-                            }
-                            if c == 's' {
-                                self.cursor.move_down();
-                            }
-                            if c == 'd' {
-                                self.cursor.move_right();
-                            }
+        match read() {
+            Ok(Key(event)) => {
+                if let Char(c) = event.code {
+                    if self.mode == Mode::Normal {
+                        if c == 'q' {
+                            return;
                         }
-                        // condition for normal mode
-                        else if self.mode == Mode::Editing {
-                            //TODO implement functionality for insert mode(exiting insert mode)
-                            if c == 'q' {
-                                self.mode = Mode::Normal;
-                            } else {
-                                self.insert_char(c);
-                            }
+                        if c == 'i' {
+                            self.mode = Mode::Editing;
                         }
-                    } else if let Backspace = event.code {
-                        if self.mode == Mode::Editing {
-                            self.buffer.pop();
+                        //TODO use custom keybindings. create another directory and move these
+                        //into a new function
+                        if c == 'w' {
+                            self.cursor.move_up();
+                        }
+                        if c == 'a' {
+                            self.cursor.move_left();
+                        }
+                        if c == 's' {
+                            self.cursor.move_down();
+                        }
+                        if c == 'd' {
+                            self.cursor.move_right();
                         }
                     }
-                    //TODO Move this into another function which is specifically built for display
-                    execute!(stdout(), Clear(ClearType::All)).ok();
-                    let line: String = self.buffer.iter().collect();
-                    execute!(stdout(), Print(line)).ok();
-                    //execute!(stdout(), Print("\r")).ok();
-                    //println!("{:?}\r", self.buffer);
-                    //execute!(stdout(), MoveTo(0, 0)).ok();
+                    // condition for normal mode
+                    else if self.mode == Mode::Editing {
+                        //TODO implement functionality for insert mode(exiting insert mode)
+                        if c == 'q' {
+                            self.mode = Mode::Normal;
+                        } else {
+                            self.insert_char(c);
+                        }
+                    }
+                } else if let Backspace = event.code {
+                    if self.mode == Mode::Editing {
+                        self.buffer.pop();
+                    }
                 }
-                Err(err) => println!("Error: {err}"),
-                _ => (),
             }
+            Err(err) => println!("Error: {err}"),
+            _ => (),
         }
-        disable_raw_mode().unwrap();
     }
     pub fn insert_char(&mut self, c: char) {
         self.buffer.push(c);
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,48 @@
 mod buffer;
 mod cursor;
 mod editor;
+use crate::editor::Mode;
 use editor::Editor;
-fn main() {
-    let mut editor = Editor::default();
-    editor.run();
+use ratatui::{
+    layout::{Constraint, Layout, Position},
+    text::{Line, Span, Text},
+    widgets::{Block, Paragraph},
+    DefaultTerminal, Frame,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let terminal = ratatui::init();
+    let app_result = App::default().run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+pub struct App {
+    editor: Editor,
+}
+
+impl App {
+    pub fn default() -> Self {
+        App {
+            editor: Editor::default(),
+        }
+    }
+
+    pub fn run(&mut self, mut terminal: DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.editor.run();
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        //let layout = Layout::default();
+        //use frame.area() to use the whole terminal
+        let line: String = self.editor.buffer.iter().collect();
+        let output = match self.editor.mode {
+            Mode::Normal => "Normal".to_string(),
+            Mode::Editing => "Editing".to_string(),
+        };
+        frame.render_widget(Paragraph::new(line), frame.area())
+    }
 }
